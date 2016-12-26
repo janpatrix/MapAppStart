f58383abfa89b8bd769a9f3d85e0b9aa8ebfe8e2
Patrick Gross <patrick.gross@native-instruments.de>
2016-12-27 00:47:17 +0100

    Added buttons

--- MyApplication/app/src/main/java/com/example/patrickgross/mapappstart/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/patrickgross/mapappstart/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.patrickgross.mapappstart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    //Map variables
+    private static boolean mapReady = false;
+    private static GoogleMap m_Map = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting up Buttons
+        final Button btnMap = (Button) findViewById(R.id.btn_map);
+        final Button btnSatellite = (Button) findViewById(R.id.btn_sat);
+        final Button btnHybrid = (Button) findViewById(R.id.btn_hyb);
+
+        //TODO Adding ClickListeners
+        btnMap.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                m_Map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        btnSatellite.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                m_Map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
+
+        btnHybrid.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                m_Map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
+        m_Map = googleMap;
+        LatLng berlin = new LatLng(52.4912047, 13.4307885);
+        CameraPosition target = CameraPosition.builder().target(berlin).zoom(17).tilt(65).build();
+        m_Map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
     }
 }
